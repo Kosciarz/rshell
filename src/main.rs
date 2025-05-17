3796276dfbb3fa82e07390a4ea427bdc5f088d75
Bartosz <bartoszbachul1@gmail.com>
2025-05-17 20:07:53 +0200

    Add command execution functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    io::{self, Write},
+    process::Command,
+    result::Result,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    print!("rshell> ");
+    io::stdout().flush()?;
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command)?;
+    let command: &str = command.trim();
+
+    let output = Command::new("powershell")
+        .arg("-Command")
+        .arg(command)
+        .output()?;
+
+    if output.status.success() {
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+    } else {
+        eprint!("{}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    Ok(())
 }
